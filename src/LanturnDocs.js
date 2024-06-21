99dc11f2392d59b1e3fbc01bab24cb37df771a4c
lanturnalis <96697794+lanturnalis@users.noreply.github.com>
2024-06-21 10:54:09 -0500

    Fix addon menu initialization and add new sort orders

--- src/LanturnDocs.js
+++ src/LanturnDocs.js
@@ -1,6 +1,13 @@
 function onOpen() {
-  SpreadsheetApp.getUi()
+  DocumentApp.getUi()
       .createAddonMenu()
-      .addItem('Sort at Cursor', 'sortAtCursor')
+      .addItem('Sort at Cursor (A-Z)', 'sortAtCursorAtoZ')
+      .addItem('Sort at Cursor (Z-A)', 'sortAtCursorZtoA')
       .addToUi();
+}
+function sortAtCursorAtoZ() {
+  sortAtCursorAsc(1)
+}
+function sortAtCursorZtoA() {
+  sortAtCursorAsc(-1)
 }
